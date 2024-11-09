2d19a0498092bdd37830728a8803e64ab4e6e434
Muhammad-Qassim <Muhammadqassim92@gmail.com>
2024-11-08 18:18:52 -0600

    Redesign SignUp page to match the UI and fixed minor issues

--- frontend/src/components/signup/SignUp.js
+++ frontend/src/components/signup/SignUp.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../firebase';
-import "./SignUp.css"
+import logo from "../img/logo3.png";
+import { Link } from "react-router-dom";
 
-function SignUp({ changeScene }) {
+function SignUp( ) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -14,67 +15,65 @@ function SignUp({ changeScene }) {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       console.log(userCredential);
       alert("User Creation Successful");
-      changeScene("mainmenu");
+      alert("Login Successful");
+      window.location.href = "/scene";
     } catch (error) {
       console.log('Error logging in:', error.code);
     }
   };
 
-  const handleChangeToLogin = () => {
-    changeScene("login");
-  }
 
-  useEffect(() => {
-    // Add CSS CDN for Semantic UI (as an example)
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.css';
-    document.head.appendChild(link);
-
-    // Clean up after component unmounts
-    return () => {
-        document.head.removeChild(link);
-      };
-  }, []);
-
-
+  {/* Got inspo from https://mdbootstrap.com/docs/standard/extended/login/ */}
   return (
-    <div className='signup'>
-        <div className="ui segment grey signup-form">
-            <form className="ui form" onSubmit={signUpButtonPressed}>
-            <div className="ui stacked segment">
-                <div className="field">
-                <div className="ui left icon input">
-                    <i className="mail icon"></i>
-                    <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="email"
-                    />
-                </div>
-                </div>
-                <div className="field">
-                <div className="ui left icon input">
-                    <i className="lock icon"></i>
-                    <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    id="password"
-                    />
-                </div>
-                </div>
-                <button className="ui button green fluid" type="submit" id="signup-btn">CREATE ACCOUNT</button>
+    <section className="login">
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="login-form col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form onSubmit={signUpButtonPressed}>
+            <div className="header">
+              <div className="text">
+                <h1 className="font-only-heading">REGISTER</h1>
+              </div>
+              <div className="underline"></div>
             </div>
+
+              <div className="form-outline mb-4">
+                <input
+                  type="email"
+                  id="form3Example3"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-outline mb-3">
+                <input
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button
+                  type="submit"
+                  className="btn btn-light hero-learn-btn-2"
+                >
+                  Register
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0 text-white">
+                 Already have an account? <Link to="/Login" className="btn btn-link-light p-0">Login Now</Link>
+                </p>
+              </div>
             </form>
-            <div className="ui message small">
-            Already have an account? <button onClick={handleChangeToLogin}>Log In</button>
-            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
